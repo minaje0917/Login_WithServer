5f8f9973153111e4ab78471a5d45372cb8fcff14
minaje0917 <k050918@naver.com>
2022-06-11 18:22:39 +0900

    start

--- Login_WithServer/ViewController.swift
+++ Login_WithServer/ViewController.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
+import Then
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
     }
 
 
