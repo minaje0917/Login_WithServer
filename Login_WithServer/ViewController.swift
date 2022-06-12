1aab839bf180728db49c1fb2fc97bbd8d9151f1b
minaje0917 <k050918@naver.com>
2022-06-12 23:09:17 +0900

    setting

--- Login_WithServer/ViewController.swift
+++ Login_WithServer/ViewController.swift
@@ -7,15 +7,33 @@
 
 import UIKit
 import Then
+import SnapKit
 
 class ViewController: UIViewController {
+    private let bounds = UIScreen.main.bounds
 
+    lazy var startButton = UIButton().then {
+        $0.setTitle("시작하기", for: .normal)
+        $0.backgroundColor = .black.withAlphaComponent(0.8)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        
+        setLayout()
+    }
+    
+    private func addView() {
+        [startButton].forEach {
+            view.addSubview($0)
+        }
     }
 
+    private func setLayout() {
+        startButton.snp.makeConstraints {
+            $0.size.equalTo(bounds.height * 0.1)
+        }
+    }
+    
 
 }
 
