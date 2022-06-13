60b8f82a7a068242ac1809749b30d632f503bd9a
minaje0917 <k050918@naver.com>
2022-06-13 16:59:04 +0900

    버튼 구현

--- Login_WithServer/ViewController.swift
+++ Login_WithServer/ViewController.swift
@@ -9,29 +9,47 @@ import UIKit
 import Then
 import SnapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     private let bounds = UIScreen.main.bounds
 
-    lazy var startButton = UIButton().then {
-        $0.setTitle("시작하기", for: .normal)
+    lazy var signUpButton = UIButton().then {
+        $0.setTitle("회원가입", for: .normal)
         $0.backgroundColor = .black.withAlphaComponent(0.8)
+        $0.layer.cornerRadius = 10
     }
+    
+    lazy var signInButton = UIButton().then {
+        $0.setTitle("로그인", for: .normal)
+        $0.backgroundColor = .black.withAlphaComponent(0.8)
+        $0.layer.cornerRadius = 10
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        addView()
         setLayout()
     }
     
     private func addView() {
-        [startButton].forEach {
+        [signUpButton, signInButton].forEach {
             view.addSubview($0)
         }
     }
 
     private func setLayout() {
-        startButton.snp.makeConstraints {
-            $0.size.equalTo(bounds.height * 0.1)
-        }
+        signUpButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.size.equalTo(bounds.height * 0.07)
+            $0.trailing.equalToSuperview().offset(-30)
+            $0.top.equalToSuperview().offset(500)
+            }
+        signInButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.size.equalTo(bounds.height * 0.07)
+            $0.trailing.equalToSuperview().offset(-30)
+            $0.top.equalToSuperview().offset(570)
+            }
     }
     
 
